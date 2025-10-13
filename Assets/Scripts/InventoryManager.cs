72e193e2f04b72faef53e3538d7033cdd3eff90f
lim0507 <ljs417848@gmail.com>
2025-10-13 15:40:22 +0900

    10.13

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+
+    [Header("Inventory Setting")]
+    public int inventorySize = 20;
+    public GameObject inventoryUI;
+    public Transform itemSlotPatent;
+    public GameObject itemSlotPrefab;
+
+    [Header("Input")]
+    public KeyCode inventoryKey = KeyCode.I;
+    private List<InventorySlot> slots = new List<InventorySlot>();
+    private bool isInventoryOpen = false;
+
+    void Start()
+    {
+        CreateInventorySlots();
+        inventoryUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(inventoryKey))
+        {
+            ToggleInventory();
+        }
+    }
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    void CreateInventorySlots()
+    {
+        for (int i = 0; i<inventorySize; i++)
+        {
+            GameObject slotObj = Instantiate(itemSlotPrefab, itemSlotPatent);
+            InventorySlot slot = slotObj.GetComponent<InventorySlot>();
+            slots.Add(slot);
+        }
+    }
+    public void ToggleInventory()
+    {
+        isInventoryOpen = !isInventoryOpen;
+        inventoryUI.SetActive(isInventoryOpen);
+
+        if(isInventoryOpen)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    public bool AddItem(ItemData item, int amount =1)
+    {
+        foreach(InventorySlot slot in slots)
+        {
+            if(slot.item == item && slot.amount < item.maxStack)
+            {
+                int spaceLeft = item.maxStack - slot.amount;
+                int amountToAdd = Mathf.Min(amount, spaceLeft);
+                slot.AddAmount(amountToAdd);
+                amount -= amountToAdd;
+
+                if(amount <= 0)
+                {
+                    return true;
+                }
+            }
+        }
+        foreach(InventorySlot slot in slots)
+        {
+            if(slot.item == null)
+            {
+                slot.SetItem(item, amount);
+                return true;
+            }
+        }
+
+        Debug.Log("인벤토리가 가득 참");
+        return false;
+    }
+    public void RemoveItem(ItemData item , int amount =1)
+    {
+        foreach(InventorySlot slot in slots)
+        {
+            if(slot.item == item)
+            {
+                slot.RemoveAmount(amount);
+                return;
+            }
+        }
+    }
+    public int GetItemCount(ItemData item)
+    {
+        int count = 0;
+        foreach (InventorySlot slot in slots)
+        {
+            if(slot.item == item)
+            {
+                count += slot.amount;
+            }
+        }
+        return count;
+    }
+
+}
---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class ItemData : ScriptableObject
+{
+    public string itemNaem;
+    public Sprite itemIcon;
+    public int maxStack = 99;
+}
